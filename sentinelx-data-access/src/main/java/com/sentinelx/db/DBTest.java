53a47cfe049d94525eafd152074701e6817303ae
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-20 22:09:15 +0530

    Project Setup Done

--- sentinelx-data-access/src/main/java/com/sentinelx/db/DBTest.java
+++ sentinelx-data-access/src/main/java/com/sentinelx/db/DBTest.java
@@ -3,5 +3,12 @@ package com.sentinelx.db;
 import java.sql.Connection;
 
 public class DBTest {
+    public static void main(String[] args) {
+        try (Connection con = DBConnection.getConnection()) {
+            System.out.println("Database Connected Successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
